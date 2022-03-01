f62df269b6d1abed117d323d8d69fec1533fc59e
J.Paulo <paulofilho0807@gmail.com>
2022-02-28 23:36:27 -0300

    ajustando o admin com campos para selecionar se e para exibir e adicionando campo para receber fotos

--- receita/admin.py
+++ receita/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from .models import Receita
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id','nome_receita')
+    list_display = ('id','nome_receita', 'categoria', 'publicar', 'date_receita')
     list_display_links = ('id','nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
-    list_per_page = 3
+    list_editable = ('publicar',)
+    list_per_page = 10
 
 admin.site.register(Receita, ListandoReceitas)
