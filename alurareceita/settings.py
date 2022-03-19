3822945a07591189c76a4f07f186e766fb5f8a41
J.Paulo <paulofilho0807@gmail.com>
2022-03-18 22:56:35 -0300

    create and configure routes

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios',
     'pessoa',
     'receita',
     'django.contrib.admin',
---
+++ usuarios/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import URLPattern, path
+from . import views
+
+urlpatterns =[
+    path('cadastro', views.cadastro, name = 'cadastro'),
+    path('login', views.login, name = "login"),
+    path('dashboard',views.dashboard,name='dashboard')
+]
