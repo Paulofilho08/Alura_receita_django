aea6fa02754b17195c7e7b42eacf40d31bef14f5
J.Paulo <paulofilho0807@gmail.com>
2022-03-29 23:20:22 -0300

    Formulario Receita

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,7 @@
-
-import re
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from receita.models import Receita
 # Create your views here.
 def cadastro(request):
     if request.method == 'POST':
@@ -11,21 +10,20 @@ def cadastro(request):
         password = request.POST['password']
         password2 = request.POST['password2']
         if not nome.strip():
-            print("existe")
+          
             return redirect('cadastro')
         if not email.strip():
-            print("tire os espaços em branco")
+           
             return redirect('cadastro')
         if password != password2:
-            print("Opa senhas não correspodem")
+            
             return redirect('cadastro')
         if User.objects.filter(email=email).exists():
-            print("Já tem ")
+           
             return redirect('cadastro')
         user = User.objects.create_user(username=nome,email=email,password=password)      
         user.save()
-        print("cadastrato") 
-        print(nome,email,password,password2)     
+        
         return redirect('login')
     else:    
         return render(request, 'usuarios/cadastro.html')
@@ -34,25 +32,50 @@ def login(request):
         email = request.POST['email']
         senha = request.POST['senha']
         if email == "" or senha == "":
-            print('Os campos email e senha não podem ficar em branco')
+         
             return redirect('login')
-        print(email, senha)
+      
         if User.objects.filter(email=email).exists():
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
             user = auth.authenticate(request, username=nome, password=senha)
             if user is not None:
                 auth.login(request, user)
-                print('deu certo')
+                
         return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 def logout(request):
     auth.logout(request)
     return redirect('index')
 def dashboard(request): 
-
     if request.user.is_authenticated:
-        return render(request,'usuarios/dashboard.html')
+         
+         id = request.user.id
+         
+         receitas = Receita.objects.order_by('-date_receita').filter(pessoa=id)
+        
+         dados = {
+             'receitas':receitas
+        }
+         
+         return render(request,'usuarios/dashboard.html', dados)
+         
     else:   
-        return  redirect('indexs')
+        return  redirect('index')
 def cria_receita(request):
-    return render(request,'usuarios/cria_receita.html')
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes =  request.POST['ingredientes']
+        modo_preparo =request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        #buscar a pessoa que está cadastrando
+        user = get_object_or_404(User, pk=request.user.id)  
+        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes
+        ,modo_preparo=modo_preparo, tempo_preparo=tempo_preparo,rendimento=rendimento,categoria=categoria,
+        foto_receita=foto_receita )
+        receita.save()
+        return redirect('dashboard')
+    else:    
+      return render(request,'usuarios/cria_receita.html')
